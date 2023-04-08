abdd857fc067ac3ce12fe06e2dd13faa1bf2f31d
haden <80521474+thdefn@users.noreply.github.com>
2023-04-09 00:07:26 +0900

    test : TimeUtil 테스트 추가

--- user-api/src/main/java/zerobase/bud/common/util/TimeUtil.java
+++ user-api/src/main/java/zerobase/bud/common/util/TimeUtil.java
@@ -11,7 +11,7 @@ public class TimeUtil {
     private static final int MIN = 60;
     private static final int HOUR = 24;
     private static final int DAY = 30;
-    private static final int MONTH = 60;
+    private static final int MONTH = 12;
 
     public static String caculateTerm(LocalDateTime date) {
         long term = Duration.between(date, LocalDateTime.now()).getSeconds();
@@ -26,7 +26,7 @@ public class TimeUtil {
         } else if ((term /= DAY) < MONTH) {
             return term + "달 전";
         } else {
-            return term/MONTH + "년 전";
+            return term / MONTH + "년 전";
         }
     }
 }
