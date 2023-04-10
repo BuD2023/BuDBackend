33770ad562f88c6d5bef5d2804c1c2173432c5c4
haden <80521474+thdefn@users.noreply.github.com>
2023-04-10 20:54:44 +0900

    hotfix : web config 수정

--- user-api/src/main/java/zerobase/bud/config/WebConfig.java
+++ user-api/src/main/java/zerobase/bud/config/WebConfig.java
@@ -14,6 +14,7 @@ public class WebConfig implements WebMvcConfigurer {
                 .allowedOrigins("http://localhost:8080/")
                 .allowedOrigins("http://localhost:5173/")
                 .allowedMethods("GET", "POST", "DELETE", "PUT")
+                .allowedHeaders("*")
                 .allowCredentials(true);
     }
 }
