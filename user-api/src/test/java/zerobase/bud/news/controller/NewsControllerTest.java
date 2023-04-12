41b935205701fdf9fce9728ef23325fbc67bda9d
xoals25 <xoals25@naver.com>
2023-04-12 21:47:22 +0900

    test: 뉴스 변경 사항 테스트 코드 적용 완료

--- user-api/src/test/java/zerobase/bud/news/controller/NewsControllerTest.java
+++ user-api/src/test/java/zerobase/bud/news/controller/NewsControllerTest.java
@@ -11,18 +11,25 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.restdocs.RestDocumentationContextProvider;
 import org.springframework.restdocs.RestDocumentationExtension;
+import org.springframework.restdocs.payload.JsonFieldType;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 import org.springframework.web.filter.CharacterEncodingFilter;
 import zerobase.bud.common.exception.BudException;
 import zerobase.bud.domain.Member;
+import zerobase.bud.news.domain.News;
 import zerobase.bud.news.dto.NewsDto;
+import zerobase.bud.news.dto.SearchAllNews;
 import zerobase.bud.news.service.NewsService;
 import zerobase.bud.jwt.TokenProvider;
 import zerobase.bud.type.MemberStatus;
@@ -38,6 +45,8 @@ import static org.mockito.BDDMockito.given;
 import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
 import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.documentationConfiguration;
 import static org.springframework.restdocs.operation.preprocess.Preprocessors.*;
+import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
+import static org.springframework.restdocs.payload.PayloadDocumentation.relaxedResponseFields;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
@@ -62,48 +71,14 @@ public class NewsControllerTest {
 
     private static final String token = "token";
 
-    @BeforeEach
-    void init(
-            WebApplicationContext context,
-            RestDocumentationContextProvider contextProvider) {
-
-        this.mockMvc = MockMvcBuilders.webAppContextSetup(context)
-                .apply(documentationConfiguration(contextProvider))
-                .addFilters(new CharacterEncodingFilter("UTF-8", true))
-                .alwaysDo(print())
-                .build();
-
-        Member member = Member.builder()
-                .id(1L)
-                .createdAt(LocalDateTime.now())
-                .status(MemberStatus.VERIFIED)
-                .email("email@naver.com")
-                .profileImg("abcde.jpg")
-                .nickname("엄탱")
-                .job("백")
-                .oAuthAccessToken("token")
-                .build();
-
-        this.objectMapper.setVisibility(PropertyAccessor.FIELD,
-                JsonAutoDetect.Visibility.ANY);
-
-        Authentication authentication =
-                new UsernamePasswordAuthenticationToken(member, "",
-                        List.of(MemberStatus.VERIFIED.getKey()).stream().map(
-                                        SimpleGrantedAuthority::new)
-                                .collect(Collectors.toList()));
-
-        given(this.tokenProvider.getAuthentication("token"))
-                .willReturn(authentication);
-    }
-
     @Test
+    @WithMockUser
     @DisplayName("뉴스 리스트 검색 성공")
     void successSearchNews() throws Exception {
         //given
-        List<NewsDto> list = new ArrayList<>();
-        for (int i = 1; i <= 10; i++) {
-            NewsDto newsDto = NewsDto.builder()
+        List<News> list = new ArrayList<>();
+        for (int i = 1; i <= 3; i++) {
+            News news = News.builder()
                     .id(i)
                     .registeredAt(getAnyDate())
                     .title("기사 제목" + i)
@@ -116,42 +91,83 @@ public class NewsControllerTest {
                     .keywords("[\"프론트엔드\", \"백엔드\"]")
                     .hitCount(i)
                     .build();
-            list.add(newsDto);
+            list.add(news);
         }
 
-//        list.sort(Comparator.comparing(NewsDto::getHitCount).reversed());
-//        list = list.subList(0, 7);
-        given(newsService.getNewsList(any())).willReturn(list);
+        PageRequest pageRequest = PageRequest.of(0, 3);
+        Page<News> newsPage = new PageImpl<>(list, pageRequest, 3);
+
+        given(newsService.getNewsList(any()))
+                .willReturn(SearchAllNews.Response.fromEntitesPage(newsPage));
 
         //when
         //then
         mockMvc.perform(get("/news")
-                        .param("size", "7")
+                        .param("size", "3")
                         .param("page", "0")
                         .param("sort", "HIT")
+                        .param("order", "ASC")
                         .param("startDate", "2000-01-20")
                         .param("endDate", "2023-03-28")
                         .param("keyword", "제목")
                 )
                 .andDo(print())
                 .andExpect(status().isOk())
-                .andExpect(jsonPath("$[0].registeredAt").value(getAnyDate().toString()))
-                .andExpect(jsonPath("$[0].title").value("기사 제목1"))
-                .andExpect(jsonPath("$[0].link").value("http://news/url/1"))
-                .andExpect(jsonPath("$[0].summaryContent").value("본문 요약 내용입니다.1"))
-                .andExpect(jsonPath("$[0].mainImgUrl").value("http://news/main/img/url/1"))
-                .andExpect(jsonPath("$[0].company").value("네이버뉴스1"))
-                .andExpect(jsonPath("$[0].keywords").value("[\"프론트엔드\", \"백엔드\"]"))
-                .andExpect(jsonPath("$[0].hitCount").value(1))
-                .andExpect(jsonPath("$[0].id").value(1))
+                .andExpect(jsonPath("content[0].registeredAt").value(getAnyDate().toString()))
+                .andExpect(jsonPath("content[0].title").value("기사 제목1"))
+                .andExpect(jsonPath("content[0].link").value("http://news/url/1"))
+                .andExpect(jsonPath("content[0].summaryContent").value("본문 요약 내용입니다.1"))
+                .andExpect(jsonPath("content[0].mainImgUrl").value("http://news/main/img/url/1"))
+                .andExpect(jsonPath("content[0].company").value("네이버뉴스1"))
+                .andExpect(jsonPath("content[0].keywords").value("[\"프론트엔드\", \"백엔드\"]"))
+                .andExpect(jsonPath("content[0].hitCount").value(1))
+                .andExpect(jsonPath("content[0].id").value(1))
                 .andDo(
                         document("{class-name}/{method-name}",
                                 preprocessRequest(prettyPrint()),
-                                preprocessResponse(prettyPrint()))
+                                preprocessResponse(prettyPrint()),
+                                relaxedResponseFields(
+                                        fieldWithPath("content[].registeredAt").type(JsonFieldType.STRING)
+                                                .description("기사 등록일"),
+                                        fieldWithPath("content[].title").type(JsonFieldType.STRING)
+                                                .description("기사 제목"),
+                                        fieldWithPath("content[].link").type(JsonFieldType.STRING)
+                                                .description("기사 링크"),
+                                        fieldWithPath("content[].summaryContent").type(JsonFieldType.STRING)
+                                                .description("기사 요약 내용"),
+                                        fieldWithPath("content[].mainImgUrl").type(JsonFieldType.STRING)
+                                                .description("기사 메인 이미지 링크"),
+                                        fieldWithPath("content[].company").type(JsonFieldType.STRING)
+                                                .description("기사 회사명"),
+                                        fieldWithPath("content[].journalistOriginalNames").type(JsonFieldType.STRING)
+                                                .description("기사 파싱되지 않은 기자(명)들"),
+                                        fieldWithPath("content[].journalistNames").type(JsonFieldType.STRING)
+                                                .description("기사 파싱된 기자(명)들"),
+                                        fieldWithPath("content[].keywords").type(JsonFieldType.STRING)
+                                                .description("검색된 키워드들"),
+                                        fieldWithPath("content[].hitCount").type(JsonFieldType.NUMBER)
+                                                .description("기사 조회수"),
+                                        fieldWithPath("first").type(JsonFieldType.BOOLEAN)
+                                                .description("첫번째 페이지인지 여부"),
+                                        fieldWithPath("last").type(JsonFieldType.BOOLEAN)
+                                                .description("마지막 페이지인지 여부"),
+                                        fieldWithPath("totalElements").type(JsonFieldType.NUMBER)
+                                                .description("검색 데이터 전체 개수"),
+                                        fieldWithPath("totalElements").type(JsonFieldType.NUMBER)
+                                                .description("검색 데이터 전체 개수"),
+                                        fieldWithPath("totalPages").type(JsonFieldType.NUMBER)
+                                                .description("검색 데이터 전체 페이지 수"),
+                                        fieldWithPath("size").type(JsonFieldType.NUMBER)
+                                                .description("요청 데이터 수"),
+                                        fieldWithPath("numberOfElements").type(JsonFieldType.NUMBER)
+                                                .description("현재 페이지에서 보여지는 데이터 수")
+                                )
+                        )
                 );
     }
 
     @Test
+    @WithMockUser
     @DisplayName("뉴스 상세 정보 검색 성공")
     void successSearchNewsDetail() throws Exception {
         //given
@@ -196,6 +212,7 @@ public class NewsControllerTest {
     }
 
     @Test
+    @WithMockUser
     @DisplayName("뉴스 디테일 데이터 검색 실패")
     void failGetNews() throws Exception {
         //given
