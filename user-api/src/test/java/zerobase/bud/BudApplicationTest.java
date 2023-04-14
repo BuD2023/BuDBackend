c84d0a80adcb2dbda1690c86b99eedf5de8b0cc1
haden <80521474+thdefn@users.noreply.github.com>
2023-04-14 13:47:06 +0900

    chore : user-api에 누락된 Redis 설정 추가

---
+++ user-api/src/test/java/zerobase/bud/BudApplicationTest.java
@@ -0,0 +1,13 @@
+package zerobase.bud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BudApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
