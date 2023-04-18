891fa70add7c31eb781d157a7ce2bd8015a199c9
xoals25 <xoals25@naver.com>
2023-04-19 02:07:10 +0900

    test: 답글 조회 및 삭제 테스트 코드 작성

--- user-api/src/test/java/zerobase/bud/post/controller/QnaAnswerControllerTest.java
+++ user-api/src/test/java/zerobase/bud/post/controller/QnaAnswerControllerTest.java
@@ -1,30 +1,43 @@
 package zerobase.bud.post.controller;
 
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.BDDMockito.given;
 import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
-import static org.springframework.restdocs.operation.preprocess.Preprocessors.preprocessRequest;
-import static org.springframework.restdocs.operation.preprocess.Preprocessors.preprocessResponse;
-import static org.springframework.restdocs.operation.preprocess.Preprocessors.prettyPrint;
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.*;
+import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
+import static org.springframework.restdocs.payload.PayloadDocumentation.relaxedResponseFields;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.http.MediaType;
+import org.springframework.restdocs.payload.JsonFieldType;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
+import zerobase.bud.domain.Member;
 import zerobase.bud.jwt.TokenProvider;
 import zerobase.bud.post.dto.CreateQnaAnswer;
+import zerobase.bud.post.dto.SearchQnaAnswer;
 import zerobase.bud.post.dto.UpdateQnaAnswer;
 import zerobase.bud.post.service.QnaAnswerService;
+import zerobase.bud.post.type.QnaAnswerStatus;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 @WebMvcTest(QnaAnswerController.class)
 @AutoConfigureRestDocs
@@ -43,6 +56,15 @@ class QnaAnswerControllerTest {
     private MockMvc mockMvc;
 
     private static final String TOKEN = "Bearer token";
+
+    @Value("${develop.server.scheme}")
+    private String scheme;
+
+    @Value("${develop.server.host}")
+    private String host;
+
+    @Value("${develop.server.port}")
+    private int port;
 
     @Test
     @WithMockUser
@@ -96,5 +118,124 @@ class QnaAnswerControllerTest {
                     preprocessRequest(prettyPrint()),
                     preprocessResponse(prettyPrint()))
             );
+    }
+
+    @Test
+    @WithMockUser
+    @DisplayName("success search list QnaAnswer")
+    void success_searchQnaAnswers() throws Exception{
+        //given
+        List<SearchQnaAnswer.Response> list = new ArrayList<>();
+
+        for (long i = 1; i <= 3; i++) {
+            list.add(SearchQnaAnswer.Response.builder()
+                    .id(i)
+                    .member(null)
+                    .content("내용입니다" + i)
+                    .commentCount(i)
+                    .likeCount(i)
+                    .qnaAnswerStatus(QnaAnswerStatus.ACTIVE)
+                    .isQnaAnswerPin(i == 1)
+                    .createdAt(LocalDateTime.now().minusDays(1))
+                    .updatedAt(LocalDateTime.now())
+                    .build());
+        }
+
+        given(qnaAnswerService.searchQnaAnswers(anyLong(), any()))
+                .willReturn(new PageImpl<>(list, PageRequest.of(0, 3), 10));
+
+        //when
+        //then
+        mockMvc.perform(get("/posts/qna-answer")
+                        .param("postId", "1")
+                        .param("page", "0")
+                        .param("size", "3")
+                        .param("sort", "DATE,DESC")
+                        .header("Authorization", TOKEN)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .with(csrf()))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("content[0].content").value("내용입니다1"))
+                .andExpect(jsonPath("content[0].commentCount").value(1))
+                .andExpect(jsonPath("content[0].likeCount").value(1))
+                .andExpect(jsonPath("content[0].qnaAnswerStatus").value(QnaAnswerStatus.ACTIVE.toString()))
+                .andExpect(jsonPath("content[0].qnaAnswerPin").value(true))
+                .andExpect(jsonPath("content[1].qnaAnswerPin").value(false))
+                .andExpect(jsonPath("content[2].qnaAnswerPin").value(false))
+                .andDo(
+                        document("{class-name}/{method-name}",
+                                preprocessRequest(
+                                        modifyUris().scheme(scheme).host(host).port(port),
+                                        prettyPrint()),
+                                preprocessResponse(prettyPrint()),
+                                relaxedResponseFields(
+                                        fieldWithPath("content[].id").type(JsonFieldType.NUMBER)
+                                                .description("qna 답글 id"),
+                                        fieldWithPath("content[].member").type(JsonFieldType.NULL)
+                                                .description("qna 답글 작성한 member 정보"),
+                                        fieldWithPath("content[].content").type(
+                                                        JsonFieldType.STRING)
+                                                .description("qna 답글 본문"),
+                                        fieldWithPath("content[].commentCount").type(
+                                                        JsonFieldType.NUMBER)
+                                                .description("qna 답글 댓글 수"),
+                                        fieldWithPath("content[].likeCount").type(
+                                                        JsonFieldType.NUMBER)
+                                                .description("qna 답글 좋아요 수"),
+                                        fieldWithPath("content[].qnaAnswerStatus").type(
+                                                        JsonFieldType.STRING)
+                                                .description("qna 답글 상태(게시, 삭제 등)"),
+                                        fieldWithPath("content[].qnaAnswerPin").type(
+                                                        JsonFieldType.BOOLEAN)
+                                                .description("qna 답글이 고정 답글인지 아닌지"),
+                                        fieldWithPath("content[].createdAt").type(
+                                                        JsonFieldType.STRING)
+                                                .description("qna 답글 등록일"),
+                                        fieldWithPath("content[].updatedAt").type(
+                                                        JsonFieldType.STRING)
+                                                .description("qna 답글 업데이트일"),
+                                        fieldWithPath("first").type(JsonFieldType.BOOLEAN)
+                                                .description("첫번째 페이지인지 여부"),
+                                        fieldWithPath("last").type(JsonFieldType.BOOLEAN)
+                                                .description("마지막 페이지인지 여부"),
+                                        fieldWithPath("totalElements").type(
+                                                        JsonFieldType.NUMBER)
+                                                .description("검색 데이터 전체 개수"),
+                                        fieldWithPath("totalPages").type(JsonFieldType.NUMBER)
+                                                .description("검색 데이터 전체 페이지 수"),
+                                        fieldWithPath("size").type(JsonFieldType.NUMBER)
+                                                .description("요청 데이터 수"),
+                                        fieldWithPath("numberOfElements").type(
+                                                        JsonFieldType.NUMBER)
+                                                .description("현재 페이지에서 보여지는 데이터 수")
+                                )
+                        ));
+
+    }
+
+    @Test
+    @WithMockUser
+    @DisplayName("success delete QnaAnswer")
+    void success_deleteQnaAnswer() throws Exception{
+        //given
+        //when
+        //then
+        mockMvc.perform(get("/posts/qna-answer")
+                        .param("postId", "1")
+                        .param("page", "0")
+                        .param("size", "3")
+                        .header("Authorization", TOKEN)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .with(csrf()))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andDo(
+                        document("{class-name}/{method-name}",
+                                preprocessRequest(
+                                        modifyUris().scheme(scheme).host(host).port(port),
+                                        prettyPrint()),
+                                preprocessResponse(prettyPrint())
+                        ));
     }
 }
--- user-api/src/test/java/zerobase/bud/post/service/QnaAnswerServiceTest.java
+++ user-api/src/test/java/zerobase/bud/post/service/QnaAnswerServiceTest.java
@@ -1,24 +1,18 @@
 package zerobase.bud.post.service;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
-import static zerobase.bud.common.type.ErrorCode.CHANGE_IMPOSSIBLE_PINNED_ANSWER;
-import static zerobase.bud.common.type.ErrorCode.INVALID_POST_STATUS;
-import static zerobase.bud.common.type.ErrorCode.INVALID_POST_TYPE_FOR_ANSWER;
-import static zerobase.bud.common.type.ErrorCode.INVALID_QNA_ANSWER_STATUS;
-import static zerobase.bud.common.type.ErrorCode.NOT_FOUND_NOTIFICATION_INFO;
-import static zerobase.bud.common.type.ErrorCode.NOT_FOUND_POST;
-import static zerobase.bud.common.type.ErrorCode.NOT_FOUND_QNA_ANSWER;
-import static zerobase.bud.common.type.ErrorCode.NOT_REGISTERED_MEMBER;
+import static zerobase.bud.common.type.ErrorCode.*;
 import static zerobase.bud.post.type.PostStatus.ACTIVE;
 import static zerobase.bud.post.type.PostStatus.INACTIVE;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -27,6 +21,8 @@ import org.mockito.ArgumentCaptor;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.data.domain.*;
+import org.springframework.security.test.context.support.WithMockUser;
 import zerobase.bud.common.exception.BudException;
 import zerobase.bud.domain.Member;
 import zerobase.bud.fcm.FcmApi;
@@ -36,10 +32,13 @@ import zerobase.bud.post.domain.Post;
 import zerobase.bud.post.domain.QnaAnswer;
 import zerobase.bud.post.domain.QnaAnswerPin;
 import zerobase.bud.post.dto.CreateQnaAnswer.Request;
+import zerobase.bud.post.dto.QnaAnswerDto;
+import zerobase.bud.post.dto.SearchQnaAnswer;
 import zerobase.bud.post.dto.UpdateQnaAnswer;
 import zerobase.bud.post.repository.PostRepository;
 import zerobase.bud.post.repository.QnaAnswerPinRepository;
 import zerobase.bud.post.repository.QnaAnswerRepository;
+import zerobase.bud.post.repository.QnaAnswerRepositoryQuerydslImpl;
 import zerobase.bud.post.type.PostType;
 import zerobase.bud.post.type.QnaAnswerStatus;
 import zerobase.bud.repository.MemberRepository;
@@ -61,6 +60,9 @@ class QnaAnswerServiceTest {
 
     @Mock
     private MemberRepository memberRepository;
+
+    @Mock
+    private QnaAnswerRepositoryQuerydslImpl qnaAnswerRepositoryQuerydsl;
 
     @Mock
     private FcmApi fcmApi;
@@ -323,6 +325,126 @@ class QnaAnswerServiceTest {
         //then 이런 결과가 나온다.
         assertEquals(CHANGE_IMPOSSIBLE_PINNED_ANSWER,
             budException.getErrorCode());
+    }
+
+    @Test
+    @WithMockUser
+    @DisplayName("success_search QnaAnswer list")
+    void success_searchQnaAnswers(){
+        //given
+        List<QnaAnswerDto> list = new ArrayList<>();
+
+        for (long i = 1; i <= 10; i++) {
+            list.add(QnaAnswerDto.builder()
+                    .id(i)
+                    .member(null)
+                    .content("내용입니다" + i)
+                    .commentCount(i)
+                    .likeCount(i)
+                    .qnaAnswerStatus(QnaAnswerStatus.ACTIVE)
+                    .createdAt(LocalDateTime.now().minusDays(1))
+                    .updatedAt(LocalDateTime.now())
+                    .pinId(i == 1 ? i : null)
+                    .build());
+        }
+
+        PageRequest pageable = PageRequest.of(0, 3);
+        pageable.withSort(Sort.Direction.DESC, "likeCount");
+
+        given(qnaAnswerRepositoryQuerydsl.findAllByPostIdAndQnaAnswerStatusNotLike(anyLong(), any()))
+                .willReturn(new PageImpl<>(list, pageable, 10));
+
+        //when
+        Page<SearchQnaAnswer.Response> qnaAnswersPages =
+                qnaAnswerService.searchQnaAnswers(1L, pageable);
+
+        //then
+        assertEquals("내용입니다1", qnaAnswersPages.getContent().get(0).getContent());
+        assertEquals(QnaAnswerStatus.ACTIVE, qnaAnswersPages.getContent().get(0).getQnaAnswerStatus());
+        assertEquals(1, qnaAnswersPages.getContent().get(0).getCommentCount());
+        assertEquals(3, qnaAnswersPages.getContent().get(2).getLikeCount());
+
+        assertTrue(qnaAnswersPages.getContent().get(0).isQnaAnswerPin());
+        assertFalse(qnaAnswersPages.getContent().get(1).isQnaAnswerPin());
+        assertFalse(qnaAnswersPages.getContent().get(2).isQnaAnswerPin());
+    }
+
+    @Test
+    @WithMockUser
+    @DisplayName("success delete qnaAnswer")
+    void success_deleteQnaAnswer(){
+        //given
+        given(qnaAnswerRepository.findById(anyLong()))
+                .willReturn(Optional.ofNullable(getQnaAnswer()));
+
+        ArgumentCaptor<QnaAnswer> captor =
+                ArgumentCaptor.forClass(QnaAnswer.class);
+
+        //when
+        qnaAnswerService.deleteQnaAnswer((long)1);
+
+        //then
+        verify(qnaAnswerRepository, times(1))
+                .save(captor.capture());
+
+        assertEquals(QnaAnswerStatus.INACTIVE, captor.getValue().getQnaAnswerStatus());
+    }
+
+    @Test
+    @WithMockUser
+    @DisplayName("FAIL NOT_FOUND_QNA_ANSWER")
+    void fail_deleteQnaAnswer_QnaAnswerNotFound() {
+        //given
+        given(qnaAnswerRepository.findById(anyLong()))
+                .willReturn(Optional.empty());
+
+        //when
+        BudException exception = assertThrows(BudException.class,
+                () -> qnaAnswerService.deleteQnaAnswer(1L));
+
+        //then
+        assertEquals(NOT_FOUND_QNA_ANSWER, exception.getErrorCode());
+    }
+
+    @Test
+    @WithMockUser
+    @DisplayName("FAIL CHANGE_IMPOSSIBLE_PINNED_ANSWER")
+    void fail_deleteQnaAnswer_QnaAnswerPin() {
+        //given
+        QnaAnswer qnaAnswer = getQnaAnswer();
+        qnaAnswer.setId(1L);
+
+        given(qnaAnswerRepository.findById(anyLong()))
+                .willReturn(Optional.of(qnaAnswer));
+
+        given(qnaAnswerPinRepository.findByQnaAnswerId(anyLong()))
+                .willReturn(Optional.of(getQnaAnswerPin()));
+
+        //when
+        BudException exception = assertThrows(BudException.class,
+                () -> qnaAnswerService.deleteQnaAnswer(anyLong()));
+
+        //then
+        assertEquals(CHANGE_IMPOSSIBLE_PINNED_ANSWER, exception.getErrorCode());
+    }
+
+    @Test
+    @WithMockUser
+    @DisplayName("FAIL ALREADY_DELETE_QNA_ANSWER")
+    void fail_deleteQnaAnswer_QnaAnswerAlreadyDelete() {
+        //given
+        QnaAnswer qnaAnswer = getQnaAnswer();
+        qnaAnswer.setQnaAnswerStatus(QnaAnswerStatus.INACTIVE);
+
+        given(qnaAnswerRepository.findById(anyLong()))
+                .willReturn(Optional.ofNullable(qnaAnswer));
+
+        //when
+        BudException exception = assertThrows(BudException.class,
+                () -> qnaAnswerService.deleteQnaAnswer(1L));
+
+        //then
+        assertEquals(ALREADY_DELETE_QNA_ANSWER, exception.getErrorCode());
     }
 
     private QnaAnswerPin getQnaAnswerPin() {
