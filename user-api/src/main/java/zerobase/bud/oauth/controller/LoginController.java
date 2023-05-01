2aca9fadcc8d5bcd2a302208eba755e8ea470f94
Sfer <106132097+Sfer7@users.noreply.github.com>
2023-05-02 00:11:27 +0900

    Update LoginController.java

--- user-api/src/main/java/zerobase/bud/oauth/controller/LoginController.java
+++ user-api/src/main/java/zerobase/bud/oauth/controller/LoginController.java
@@ -6,10 +6,7 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.util.ObjectUtils;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import zerobase.bud.domain.Member;
 import zerobase.bud.oauth.service.LoginService;
 
